4620db9dee0ce379df2a15bb22d047e499be8356
Ramanan15 <billaramana15@gmail.com>
2017-08-03 15:13:40 +0530

    Add files via upload

---
+++ P53.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Hunt53 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the String :");
+		String s = in.nextLine();
+		System.out.println("Enter the length of substring :");
+		int n=in.nextInt();
+		if(n<s.length()) {
+			for(int)
+		}
+
+	}
+
+}
