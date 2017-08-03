0ecca3be9dc4714075886c5dc5313349ba021f06
Ramanan15 <billaramana15@gmail.com>
2017-08-03 15:13:58 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class HuntTest2{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String ip = in.next();
+		System.out.println("Enter the length");
+		int a = in.nextInt();
+		String s4 = ip.substring(0,1);
+		for(int i = a - 1;i < ip.length();i++){
+			System.out.println(s4+ip.substring(i,i + 1));
+		}
+	}
+
+}
