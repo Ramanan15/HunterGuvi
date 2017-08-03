9cbcd981344fc47b415775ae26c9686b79eebd94
Ramanan15 <billaramana15@gmail.com>
2017-08-03 15:17:58 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,17 @@
+public class P1 {
+
+	public static void main(String[] args) {
+
+		int[] b = {4,2,3,4,2};
+
+		for (int i=0;i<b.length;i++) {
+
+			for(int j=0;j<j+1;j++) {
+				 
+			}
+			
+		}
+
+	}
+
+}
