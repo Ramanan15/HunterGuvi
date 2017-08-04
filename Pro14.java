f420fbbdc01dfede16dc2b7358f93e2cefc85946
Ramanan15 <billaramana15@gmail.com>
2017-08-04 12:08:41 +0530

    Update Pro14.java

--- Pro14.java
+++ Pro14.java
@@ -1,49 +1,34 @@
 import java.util.*;
-public class P 14
- {
-public static void main(String[] args) 
-{
-	List<String> ls=new ArrayList<String>();
+public class Pro14 {
+public static void main(String[] args) {
+	List<String> ls = new ArrayList<String>();
 	ls.add("A");
 	ls.add("AN");
-
 	ls.add("APPLE");
 	ls.add("AWAY");
 	ls.add("BALL");
-
 	ls.add("CAT");
 	ls.add("DAY");
 	ls.add("DOCTOR");
-
 	ls.add("KEEPS");
 	ls.add("THE");
 	String input = "An Apple An Day keeps the doctr keeps away";
-
 	String inputcopy = input.toUpperCase();
-	String words[]=input.split(" ");
-
+	String words[] = input.split(" ");
 	String wordscopy[]=inputcopy.split(" ");
-
 	int countnumber[]=new int[input.length()];
 	int countindexoriginal[]=new int[input.length()];
-
 	int countindexlist[]=new int[input.length()];
-
 	int g=0;
 	String str = null;
-	for( int i = 0;i < words.length ; i++)
- {
+	for( int i = 0;i < words.length ; i++) {
 		int count=0;
-		if(!(ls.contains(wordscopy[i])))
-{
+		if(!(ls.contains(wordscopy[i]))) {
 			 str=wordscopy[i];
-			for(int h=0;h<ls.size();h++)
-{
+			for(int h=0;h<ls.size();h++) {
 			String cmp=ls.get(h);	
-			for(int j=0,k=0;j<str.length()&&k<cmp.length();j++,k++)
-{
-					if(str.charAt(j)==cmp.charAt(k))
-{
+			for(int j=0,k=0;j<str.length()&&k<cmp.length();j++,k++) {
+					if(str.charAt(j)==cmp.charAt(k)) {
 						count++;
 					}
 				}	
@@ -58,10 +43,8 @@ public static void main(String[] args)
 		int max=0;
 		int maxi=0;
 
-		for(int p=0;p<g;p++)
-{
-			if(countnumber[p]>max)
-{
+		for(int p = 0;p<g;p++) {
+			if(countnumber[p]>max) {
 				maxi=p;
 				max=countnumber[p];
 			}
